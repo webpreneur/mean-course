39388cc6218cb401ebe46ef3ec8c1c70d9291621
Gulyas Zsolt <zsolt.gulyas@outlook.com>
2018-10-08 07:52:52 +0200

    Updating posts on the server (66)

--- backend/app.js
+++ backend/app.js
@@ -45,6 +45,7 @@ app.post('/api/posts', (req, res, next) => {
 
 app.put('api/posts/:id', (req, res, next) => {
   const post = new Post({
+    _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
   });
@@ -62,6 +63,16 @@ app.get('/api/posts', (req, res, next) => {
         posts: documents
       });
     })
+});
+
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if(post){
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: 'Post not found'});
+    }
+  });
 });
 
 app.delete('/api/posts/:id', (req, res, next) => {
